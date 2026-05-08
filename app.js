d874f05949cba9b1cfb16372700dcf1b699bd824
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2026-05-07 22:06:30 -0300

    Formatando os arquivos do projeto.

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
 function pesquisar() {
-    // Obtém a seção HTML onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
+  // Obtém a seção HTML onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
 
-    let campoPesquisa = document.getElementById("campo-pesquisa").value
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
-    // se campoPesquisa for uma string sem nada
-    if (!campoPesquisa) {
-        section.innerHTML = "<p>País invalido.</p>"
-        return 
-    }
+  // se campoPesquisa for uma string sem nada
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>País invalido.</p>";
+    return;
+  }
 
-    campoPesquisa = campoPesquisa.toLowerCase()
+  campoPesquisa = campoPesquisa.toLowerCase();
 
-    // Inicializa uma string vazia para armazenar os resultados
-    let resultados = "";
-    let titulo = ""; 
-    let descricao = "";
-    let tags = "";
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
 
-    // Itera sobre cada dado da lista de dados
-    for (let dado of dados) {
-        titulo = dado.titulo.toLowerCase()
-        descricao = dado.descricao.toLowerCase()
-        tags = dado.tags.toLowerCase()
-        // se titulo includes campoPesquisa
-        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
-            // cria um novo elemento
-            resultados += `
+  // Itera sobre cada dado da lista de dados
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+    // se titulo includes campoPesquisa
+    if (
+      titulo.includes(campoPesquisa) ||
+      descricao.includes(campoPesquisa) ||
+      tags.includes(campoPesquisa)
+    ) {
+      // cria um novo elemento
+      resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank" rel="noopener noreferrer">${dado.titulo}</a>
@@ -35,13 +39,13 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank" rel="noopener noreferrer">Mais informações</a>
             </div>
         `;
-        }
     }
+  }
 
-    if (!resultados) {
-        resultados = "<p>País não encontrado</p>"
-    }
+  if (!resultados) {
+    resultados = "<p>País não encontrado</p>";
+  }
 
-    // Atribui os resultados gerados à seção HTML
-    section.innerHTML = resultados;
+  // Atribui os resultados gerados à seção HTML
+  section.innerHTML = resultados;
 }
